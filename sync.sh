d5fd9e611eb1652329ed4cd8e8aaaf0ec237e41c
Miëtek Bak <mietek@bak.io>
2014-10-03 15:08:11 +0100

    Fix sync script

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-s3cmd sync out/pub/ s3://halcyon.sh --acl-public --no-preserve --add-header="Content-Encoding:gzip" --exclude='*' --include='*.gz' && \
-s3cmd sync out/pub/ s3://halcyon.sh --acl-public --no-preserve --delete-removed --exclude='*.git*'
+s3cmd sync out/pub/ s3://haskellonheroku.com --acl-public --no-preserve --add-header="Content-Encoding:gzip" --exclude='*' --include='*.gz' && \
+s3cmd sync out/pub/ s3://haskellonheroku.com --acl-public --no-preserve --delete-removed --exclude='*.git*'
